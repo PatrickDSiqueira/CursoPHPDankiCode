dabd19e78d979548eeff5740b19851311fdb3e7f
Patrick Siqueira <psique.estuda@gmail.com>
2023-02-01 15:25:07 -0300

    Seção

--- index.php
+++ index.php
@@ -1,52 +1,17 @@
 <?php
 
-// Variavél
-$nome = 'Patrick';
-echo "Meu nome é $nome";
-$nome = 'João';
-echo 'Meu nome é ' .$nome;
+// Seção 
 
-//Constante 
-define('ConstNome', 'Patrick');
-echo ConstNome;
+session_start();
 
-// Função em PhP
-
-function printarNome($nome){
-    echo '<h3>Meu nome é ' . $nome .'<h3/>';
-    echo '<br/>';
+if (!isset($_SESSION['nome'])) {
+    $_SESSION['nome'] = 'Patrick';
+} else {
+    echo 'já existe';
 }
 
-printarNome('Patrick');
-printarNome('João');
+echo $_SESSION['nome'];
 
-// funcões nativas exemplo
-
-$frase = 'O rato roeu a roupa do rei de roma, onde está o rato até hoejeu não sei';
-
-echo substr($frase, 0, 8);
-
-// Laços de repetições
-
-
-for ($i=0; $i < 100 ; $i++) { 
-    echo 'Patrick '.$i;
-    # code...
-}
-
-$a = 0;
-
-while($a <= 10) {~
-    $a++;
-    echo $a;    
-    # code...
-}
-
-$a = 0;
-do {
-    $a++;
-    echo $a;
-    
-} while ($a <= 10);
+// Essas informações da seção quando estartada ficam no servidor da aplicação por um tempo 
 
 ?>
