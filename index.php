a114fafb53dc8ebdfd69cbfbfde4bf6dde0265fc
Patrick Siqueira <psique.estuda@gmail.com>
2023-02-02 09:41:36 -0300

    Conexao com o banco de dados, insert e select exemplo

--- index.php
+++ index.php
@@ -1,10 +1,47 @@
 <?php
 
-$page = @$_GET['page'];
+include_once 'credenciais.php';
 
-include('header.php');
+class BD{
+    private $pdo;
 
-if (file_exists($page.'.php')) {
-    include($page.'.php');
+    function __construct($user, $senha){
+        $this->pdo = new PDO('mysql:host=localhost;dbname=testes', $user, $senha);
+    }
+
+    public function getConnection(){
+        return $this->pdo;
+    }
 }
+
+
+$bd = new BD(USER,SENHA);
+
+for ($i=0; $i < 10; $i++) { 
+    // Proteção de sql injec
+    $sql = $bd->getConnection()->prepare("INSERT INTO `clientes` VALUES (null,?,?)");
+    $sql->execute(array('teste', 'theu.siqueira@decathlon.com'));
+}
+
+
+$sql = $bd->getConnection()->prepare("SELECT * FROM clientes");
+
+$sql->execute();
+
+$clientes = $sql->fetchAll();
+
+foreach ($clientes as $key => $value) {
+    echo $value['nome'] . ' | ' . $value['emial'];
+    echo '<br>';
+}
+
+
+
+
+
 ?>
+
+<p>oito pilhas por um real</p>
+
+<p><?php echo USER?></p>
+<p><?php echo SENHA?></p>
